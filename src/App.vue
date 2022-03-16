87679e3d385a434358913e24c1bed79d02050bfa
unknown <taufiqur@digits.id>
2022-03-17 00:33:27 +0700

    taufiq-todolist

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container" style="margin-top: 20px;">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Todo-List Taufiq</h5>
+        <div class="row">
+          <div class="col-10">
+            <input v-model="todo" type="text" class="form-control" @keyup.enter="add"/>
+          </div>
+          <div class="col-2">
+            <button class="btn btn-success" @click="add">ADD</button>
+          </div>
+        </div>
+        <list :todos="todos" @deleteTodo="deleteTodo" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import List from "./components/List.vue";
+
+export default {
+  components: { List },
+  data() {
+    return {
+      todo: "",
+      todos: []
+    }
+  },
+  methods: {
+    add() {
+      this.todos.unshift(this.todo);
+      this.too = "";
+    },
+    deleteTodo(todoIndex) {
+      this.todos = this.todos.filter((item, index) => {
+        if (index != todoIndex) {
+          return item
+        }
+      })
+    }
+  }
+}
+</script>
---
+++ src/components/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="margin-top: 20px;">
+    <ul class="list-group">
+      <li v-for="(todo, index) in todos" :key="index" class="list-group-item">
+        <div class="row">
+          <div class="col">
+            <span>{{ todo }}</span>
+          </div>
+          <div class="col-auto">
+            <div class="row gx-2">
+              <div class="col">
+                <button @click="deleteTODO(index)" class="btn btn-danger">X</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      default: []
+    }
+  },
+  methods: {
+    deleteTODO(index) {
+      this.$emit('deleteTodo', index)
+    }
+  }
+}
+</script>
